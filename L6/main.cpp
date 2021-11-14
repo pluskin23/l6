19ceda843b1a88cb667eb2df7838a6f1295624c2
Apleon <shara49750@gmail.com>
2021-11-14 23:27:28 +0300

    Initial Commit

---
+++ L6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  L6
+//
+//  Created by Андрей Шарапов on 14.11.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
