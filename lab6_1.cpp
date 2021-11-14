e4e80d3243ed5b5fb7eeec200d13bcd5959f94bc
Apleon <shara49750@gmail.com>
2021-11-14 23:39:52 +0300

    add

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -6,4 +6,39 @@
 //
 
 #include "lab6_1.hpp"
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int k, proizv = 0;
+    cout << "¬‚Â‰ËÚÂ k = ";
+    cin >> k;
+    int array[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "array[" << i << "] = ";
+        cin >> array[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << " " << array[i] << endl;;
+    }
+    int min = array[0];
+    for (int i = 0; i < 5; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+
+    }
+    proizv = min * k;
+    cout << "proizv = \t\n" << proizv << endl;
+    system("pause");
+    return 0;
+
+}
+
 //update some program
