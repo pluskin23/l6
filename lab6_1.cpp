f74bdf095f60e0f06db98b4ac09e72f56a46538f
Apleon <shara49750@gmail.com>
2021-11-14 23:30:07 +0300

    add

---
+++ lab6_1.cpp
@@ -0,0 +1,8 @@
+//
+//  lab6_1.cpp
+//  L6
+//
+//  Created by Андрей Шарапов on 14.11.21.
+//
+
+#include "lab6_1.hpp"
---
+++ lab6_1.hpp
@@ -0,0 +1,13 @@
+//
+//  lab6_1.hpp
+//  L6
+//
+//  Created by Андрей Шарапов on 14.11.21.
+//
+
+#ifndef lab6_1_hpp
+#define lab6_1_hpp
+
+#include <stdio.h>
+
+#endif /* lab6_1_hpp */
